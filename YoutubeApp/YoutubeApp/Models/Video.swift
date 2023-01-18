0ec1879d1d3b8a28c46ca5f80797ede14e8eeedf
PatrickR <patrick@ruzman.net>
2023-01-18 13:38:04 +0100

    [Add] decodable protocol to Video and Response models

--- YoutubeApp/YoutubeApp/Models/Video.swift
+++ YoutubeApp/YoutubeApp/Models/Video.swift
@@ -7,10 +7,40 @@
 
 import Foundation
 
-struct Video {
+struct Video: Decodable {
     var video: String
     var title: String
     var description: String
     var thumbnail: String
-    var published: String
+    var published: Date
+
+    enum CodingKeys: String, CodingKey {
+        case snippet
+        case thumbnails
+        case high
+        case resourceId
+
+        case published = "publishedAt"
+        case title = "title"
+        case description = "description"
+        case thumbnail = "url"
+        case video = "videoId"
+    }
+
+    init(from decoder: Decoder) throws {
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        self.description = try snippetContainer.decode(String.self, forKey: .description)
+        self.published = try snippetContainer.decode(Date.self, forKey: .published)
+
+        let thumbnailContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+        let highContainer = try thumbnailContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+        self.thumbnail = try highContainer.decode(String.self, forKey: .thumbnail)
+
+        let resourceContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        self.video = try resourceContainer.decode(String.self, forKey: .video)
+    }
 }
